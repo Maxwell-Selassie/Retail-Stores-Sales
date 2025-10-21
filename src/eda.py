891063a2d1901c4e8792bb46f538e7a4abfafc81
Maxwell Selassie <maxwellselassie2004@gmail.com>
2025-10-21 19:13:57 +0000

    Retail stores sales

--- src/eda.py
+++ src/eda.py
@@ -1,3 +1,4 @@
+# import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import logging
 import warnings
 warnings.filterwarnings('ignore')
 
+# logging and filepaths setup
 base_dir = Path(__file__).resolve().parents[1]
 logs_dir = base_dir / 'logs'
 plot_dir = base_dir / 'plots'
@@ -23,6 +25,7 @@ logging.basicConfig(filename=log_path,
                     format='%(asctime)s - %(levelname)s : %(message)s',
                     datefmt='%H:%M:%S')
 
+# ------------load dataset from a csv file-------------
 def load_file(filename : str = 'data/retail_store_sales.csv') -> pd.DataFrame:
     '''Loads the csv file into the python environment as a pandas environment'''
     try:
@@ -32,7 +35,8 @@ def load_file(filename : str = 'data/retail_store_sales.csv') -> pd.DataFrame:
     except FileNotFoundError:
         log.error(f'File not found! Check filepath and try again!')
         return None
-    
+
+# ------a short descriptive statistical summary of the dataset----------  
 def summary_overview(df : pd.DataFrame):
     '''A short, descriptive summary of the dataset'''
     log.info(f'Number of observations : {df.shape[0]}')
@@ -44,7 +48,44 @@ def summary_overview(df : pd.DataFrame):
         'Features' : df.shape[1],
         'Description' : df.describe(include='all')
     }
+
+# -----------numerical columns - their minimum and maximum average values-----------
+def numeric_columns(df : pd.DataFrame):
+    numeric_cols = df.select_dtypes(include=[np.number]).columns
+    for i, col in enumerate(numeric_cols,1):
+        log.info(f'{i}. {col:<15} | Min : {df[col].min():<3} | Max : {df[col].max()}') 
+        return numeric_cols
+
+# ---------categorical columns - number of unique values each category contains
+def categorical_columns(df : pd.DataFrame):
+    categorical_cols = df.select_dtypes(exclude=[np.number]).columns
+    for i, col in enumerate(categorical_cols,1):
+        uniques = df[col].unique()
+        log.info(f'{i}. {col} | Unique : {df[col].nunique()} | Examples : {uniques[:4]}')
+        return categorical_cols
     
+# -----------detect outliers using the IQR method------------
+def outliers_detection(df: pd.DataFrame, col: str):
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)]
+    return lower_bound, upper_bound, outliers
+
+# ----------outlier summary - return lower range, upper range and number of outliers
+def outlier_summary(df: pd.DataFrame, numeric_cols : list[str]):
+    for col in numeric_cols:
+        lower, upper, outlier = outliers_detection(df, col)
+        outlier_df = pd.DataFrame({
+            'Lower_bound' :lower,
+            'Upper_bound' : upper,
+            'Outliers' : len(outlier)
+        })
+    return outlier_df
+
+# ----------- missing values - missing percentages -------------
 def missing_values(df : pd.DataFrame):
     '''Analyze the missing values in the dataset'''
     missing = df.isnull().sum()
@@ -55,24 +96,50 @@ def missing_values(df : pd.DataFrame):
         'missing_pct' : missing_pct.round(2)
     })
     log.info(f'{missing_df}')
-    # plt.figure(figsize=(12,7))
-    # sns.barplot(data=missing_df, y='missing_values',color='indigo',alpha=0.7)
-    # plt.title('Missing Values', fontsize=12, fontweight='bold')
-    # plt.ylabel('Frequency')
-    # plt.savefig(plot_dir / 'missing_values.png', dpi=300)
-    # plt.show()
     return missing_df
 
+# ------------plot missing values -----------
 def plt_missing_values(df : pd.DataFrame):
     df['missing_values'].plot(kind='barh',figsize=(12,7),title='Distribution of missing values',
             xlabel='Frequency')
     plt.savefig(plot_dir / 'missing_values.png', dpi=300)
+    log.info('Image successfully saved!')
     plt.show()
 
+# -----------check for duplicates in the dataset--------------
+def duplicate_data(df : pd.DataFrame):
+    duplicates = df[df.duplicated]
+    log.info(f'Number of duplicates : {len(duplicates)}')
+    if len(duplicates) == 0:
+        log.info('No duplicates found')
+    else:
+        return duplicates
+    
+def correlation_in_data(df : pd.DataFrame):
+    return df.corr(numeric_only=True, method='spearman')
+    
 
+def run_eda(filename: str = 'data/retail_stores_sales.csv'):
+    df = load_file()
+    overview = summary_overview(df)
+    missing_df = missing_values(df)
+    plt_data = plt_missing_values(missing_df)
+    num_cols = numeric_columns(df)
+    cat_cols = categorical_columns(df)
+    duplicates = duplicate_data(df)
+    outliers = outlier_summary(df, num_cols)
+
+    return {
+        'data' : df,
+        'overview' : overview,
+        'missing_df' : missing_df,
+        'plt_data' : plt_data,
+        'num_cols' : num_cols,
+        'cat_cols' : cat_cols,
+        'duplicates' : duplicates,
+        'outliers' : outliers
+    }
 
 if __name__ == '__main__':
-    df = load_file()
-    summary_overview(df)
-    missing_df = missing_values(df)
-    plt_missing_values(missing_df)
+    results = run_eda()
+    df = results['data']
